ee00cd3e7481667c4de561a289573a0b26bbf736
bycop <thebycop@gmail.com>
2021-01-11 15:13:08 +0100

    Remake all

--- index.js
+++ index.js
@@ -1,124 +1,60 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('./config.json');
-const req = require('request');
+const Gamedig = require('gamedig');
 
 bot.on('ready', () => {
-  var arg = config.ipabs
-  let api = `http://${arg}/players.json`
-  req(api, function (err, response, body) {
-  var start = JSON.parse(body)
-
-  if (start == null || start == []) {
-    var e = 0
-  } else {
-    var e = start.length;
-  }
-
-  console.log("Ready");
-  bot.user.setActivity(e+"/"+config.max);
-  })
+  Gamedig.query({
+    type: 'fivem',
+    host: config.ipabs, // This needs to be a string
+    port: config.port // This needs to be a number & is optional, unless you're not using the default port for that gameserver type
+  }).then((state) => {
+    console.log(state);
+  bot.user.setActivity(state.raw.clients+"/"+state.maxplayers);
+  }).catch((error) => {
+    console.log(error);
   });
-
+  });
 bot.on('message', (message) => {
 
-    if(message.author.bot) return;
-    if(message.content.indexOf(config.prefix) !== 0) return;
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-    if(command === "playerscount"){
-        message.delete();
-        if (config.discord === message.guild.id) {
-          try {
-            var arg = config.ipabs
-            let api = `http://${arg}/players.json`
-            let api2 = `http://${arg}/info.json`
-          req(api2, function (err, response, main) {
-              req(api, function (err, response, body) {
-                if (err) {
-                  message.channel.send("Server offline or not found")
-                }
-                else {
-                  try {
-                    var start = JSON.parse(body)
-                    var start2 = JSON.parse(main)
-      
-                    if (start == null || start == []) {
-                      var e = 0
-                    } else {
-                      var e = start.length;
-                    }
-                    message.channel.send("There is actually "+e+" players on the server.")
-                  
-                  } catch (err) {
-                    message.channel.send("Server offline or not found")
-                  }
-                }
-      
-              })
-      
-            })
-      
-          } catch (err) {
-            message.channel.send("The server doesn't exist.")
-          }
-        } else {
-          return message.channel.send("Error")
-      
-        }
-      };
-      if(command === "playerlist"){
-        //If you want to restrict the access of the command for one role only
-        let staff = message.guild.roles.cache.find(r => r.name === "Role name");
-        if(!message.member.roles.cache.has(staff.id)) return message.reply("You doesn't have the permission to show the playerlist");
-        // Delete the two lines if you don't want
-        if (config.discord === message.guild.id) {
-          try {
-            var arg = config.ipabs
-            let api = `http://${arg}/players.json`
-            let api2 = `http://${arg}/info.json`
-            req(api2, function (err, response, main) {
-              req(api, function (err, response, body) {
-                if (err) {
-                  message.channel.send("Server offline or not found")
-                }
-                else {
-                  try {
-                    var start = JSON.parse(body)
-                    var start2 = JSON.parse(main)
-      
-                    if (start == null || start == []) {
-                      var e = 0
-                    } else {
-                      var e = start.length;
-                    }
-                    let liste = '';
-                    start.forEach(function (element) {
-                      var sv = `**${element.name}**\nID: **${element.id}** Ping: **${element.ping}**`;
-                      liste = `${liste}`+"```"+`${element.name} | ${element.id} | ${element.ping} ms`+"```"+`\n`
-                    })
-                    message.channel.send(liste, { split: true })
-                  
-                  } catch (err) {
-                    console.log(err)
-                    message.channel.send("Server offline or not found")
-                  }
-                }
-      
-              })
-      
-            })
-      
-          } catch (err) {
-            message.channel.send("The server doesn't exist.")
-          }
-        } else {
-          return message.channel.send("Error")
-      
-        }
-      };
-    
+  if (command === "playerscount") {
+    message.delete();
+    Gamedig.query({
+      type: 'fivem',
+      host: config.ipabs, // This needs to be a string
+      port: config.port// This needs to be a number & is optional, unless you're not using the default port for that gameserver type
+    }).then((state) => {
+      console.log(state);
+      message.channel.send(`There is ${state.raw.clients}/${state.maxplayers} connected players.`);
+    }).catch((error) => {
+      message.channel.send(`Server offline or not found.`);
+    });
+  }
+  if (command === "playerlist") {
+    message.delete();
+    Gamedig.query({
+      type: 'fivem',
+      host: config.ipabs, // This needs to be a string
+      port: config.port// This needs to be a number & is optional, unless you're not using the default port for that gameserver type
+    }).then((state) => {
+      console.log(state);
+      let liste = '';
+      let i = 0;
+      while (i < state.raw.clients)
+      {
+        liste = `${liste}`+"```"+`${state.players[i].name} | ${state.players[i].ping} ms`+"```"+`\n`
+        i++;
+      }
+        message.channel.send(liste, { split: true })
+    }).catch((error) => {
+      message.channel.send(`Server offline or not found.`);
+    });
+  }
 })
 
 bot.login(config.token);
--- readme.md
+++ readme.md
@@ -15,22 +15,15 @@ Once you download the files and install node.js, make 'npm install' in the proje
 {
     "prefix" : "!", //Your bot prefix
     "discord" : "Discord ID", //Discord server ID where the bot is
-    "ipabs" : "Server IP:Port", //The server ip with is port
+    "ipabs" : "Server IP", //The server ip
+    "port" : "Server port (like 30120)", //The server port
     "activity" : "GameActivity", //The message display on "Playing as"
     "max" : "100", //Max users of the server
     "token" : "" //Your bot token
 }
 ```
-Don't forget to change or remove the lines Role name line 73 and 74 of index.js
-```javascript
-{
-       //If you want to restrict the access of the command for one role only
-       let staff = message.guild.roles.cache.find(r => r.name === "Role name"); //HERE
-       if(!message.member.roles.cache.has(staff.id)) return message.reply("You doesn't have the permission to show the playerlist");
-       // Delete the two lines if you don't want
-}
-```
-When you run the bot, the two commands are !playerlist and !playercount. You can change the commands and restrict or no the playerlist command in the index.js file
+
+When you run the bot, the two commands are !playerlist and !playercount.
 
 ## Others Bots
 
